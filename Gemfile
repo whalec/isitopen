c4e90396691c8d20d583898c35cf74f599cdd3e0
Cameron Barrie <cameron@snepo.com>
2011-01-10 10:05:25 +1100

    Adding time based look up

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems
 
 gem "sinatra", ">=1"
+gem 'activesupport', "=2.3.5"
 
--- isitopen.rb
+++ isitopen.rb
@@ -1,6 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+gem 'activesupport', "=2.3.5"
+require 'active_support'
+
+Time.zone = "Sydney"
+
+OPENING_DATE = Time.zone.parse("01/12/2011 07:00:00")
 
 get "/" do
+	if Time.zone.now >= OPENING_DATE
+		@result = "YES"
+	else
+		@result = "NO"
+	end
 	erb :index
 end
